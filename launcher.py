eb3c8fe7b13ed8cd265c420c0799a5032977822a
iblend171 <iblend171@gmail.com>
2024-10-20 11:14:56 -0400

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,7 +1,7 @@
 import os
 
 # Path to the Streamlit app
-app_path = r"C:\Users\Rajiv\Downloads\MRU\Trading_App\app.py"
+app_path = 'https://github.com/iblend171/Trade_Lots_POC/blob/main/app.py'
 
 # Launch Streamlit app
 os.system(f"streamlit run {app_path}")
