23557cb32e2fe07ce3c1deece7432abff1c79d25
iblend171 <iblend171@gmail.com>
2024-10-20 12:47:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Title of the Streamlit app
 st.title("CSV Data Viewer")
 
-# GitHub raw file path to auto-load the CSV
+# GitHub raw file path to auto-load
 default_file_path = (
     "https://raw.githubusercontent.com/iblend171/Trade_Lots_POC/main/OPEN_ind_tsx_350list.csv"
 )
@@ -19,6 +19,7 @@ def load_csv(file_path):
 
 # Load the CSV and display it
 df = load_csv(default_file_path)
+df = df.iloc[:,1:]
 
 if df is not None:
     st.subheader("Tabular Data:")
@@ -26,3 +27,5 @@ if df is not None:
 
     st.subheader("Summary Statistics:")
     st.write(df.describe())
+
+# print(df.head())
